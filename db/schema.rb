0db9c6eb7fd242eef0acdb5bd6fdaf29b9b78973
Tomas Duringer Jacques de Lima <tomas.lima@dtmtec.com.br>
2014-06-21 15:36:10 -0300

    Adiciona modelo Friend

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140621175854) do
+ActiveRecord::Schema.define(version: 20140621183507) do
+
+  create_table "friends", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.datetime "created_at"
