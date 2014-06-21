a4ec702d3d5d2632562c38da4e5d230a15dd6440
Tomas Duringer Jacques de Lima <tomas.lima@dtmtec.com.br>
2014-06-21 16:35:54 -0300

    Adiciona modelo Activity

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140621191445) do
+ActiveRecord::Schema.define(version: 20140621193144) do
+
+  create_table "activities", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "friends", force: true do |t|
     t.datetime "created_at"
