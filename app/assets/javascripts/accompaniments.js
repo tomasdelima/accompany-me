eb13165b45aee1b0865d650b68a44461887a3844
Tomas Duringer Jacques de Lima <tomas.lima@dtmtec.com.br>
2014-07-05 12:25:30 -0300

    Ajusta estilo

--- app/assets/javascripts/accompaniments.js
+++ app/assets/javascripts/accompaniments.js
@@ -1,2 +1,6 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(document).ready(function(){
+  $('.accompaniments').on('click', function (e) {
+     elem = $(e.currentTarget).children('ul')
+    $(elem).toggleClass('hidden')
+  })
+})
