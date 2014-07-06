76ac7f1369de2c5d0f984b7c0d70c955dc38d303
Tomas Duringer Jacques de Lima <tomas.lima@dtmtec.com.br>
2014-07-05 23:29:43 -0300

    Adiciona funções de data

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -19,9 +19,36 @@ class Activity < ActiveRecord::Base
     end
   end
 
+  def since_last_occurrence
+    if last_occurrence
+      d = (Date.today - last_occurrence.to_date).to_i
+      "(#{d} day#{'s' if d > 1} ago)"
+    end
+  end
+
   def until_next_occurrence
     if next_occurrence
       d = (next_occurrence.to_date - Date.today).to_i
+      "(in #{d} day#{'s' if d > 1})"
+    end
+  end
+
+  def since_last_accompanied
+    if last_accompanied
+      d = (Date.today - last_accompanied.to_date).to_i
+      "(#{d} day#{'s' if d > 1} ago)"
+    end
+  end
+
+  def next_accompaniment
+    if last_accompanied && accompaniment_frequency
+      last_accompanied + accompaniment_frequency.days
+    end
+  end
+
+  def until_next_accompaniment
+    if next_accompaniment
+      d = (next_accompaniment.to_date - Date.today).to_i
       "(in #{d} day#{'s' if d > 1})"
     end
   end
--- app/models/friend.rb
+++ app/models/friend.rb
@@ -6,6 +6,10 @@ class Friend < ActiveRecord::Base
 
   validates :name, presence: true
 
+  def activities_organizing
+    Activity.where(organizer: self)
+  end
+
   def since_last_accompanied
     if last_accompanied
       d = (Date.today - last_accompanied).to_i
@@ -13,7 +17,16 @@ class Friend < ActiveRecord::Base
     end
   end
 
-  def activities_organizing
-    Activity.where(organizer: self)
+  def next_accompaniment
+    if last_accompanied && accompaniment_frequency
+      last_accompanied + accompaniment_frequency.days
+    end
+  end
+
+  def until_next_accompaniment
+    if next_accompaniment
+      d = (next_accompaniment.to_date - Date.today).to_i
+      "(in #{d} day#{'s' if d > 1})"
+    end
   end
 end
