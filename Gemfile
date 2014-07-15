ffe43571233d348fea8c924c8d430302c3deef5e
Tomas Duringer Jacques de Lima <tomas.lima@dtmtec.com.br>
2014-07-15 14:42:42 -0300

    Corrige funcionamento da caixa de acompanhamento

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'bootstrap-sass'
 gem 'twitter-bootstrap-rails'
 gem 'sass-rails', '~> 4.0.0'
 gem 'uglifier', '>= 1.3.0'
+gem 'font-awesome-sass'
 
 gem 'devise'
 
--- app/assets/javascripts/accompaniments.js
+++ app/assets/javascripts/accompaniments.js
@@ -1,7 +1,11 @@
 function toggleAccompaniment () {
   $('.accompaniments').on('click', function (e) {
-     elem = $(e.currentTarget).children('ul')
-    $(elem).toggleClass('hidden')
+      var elem  = $(e.currentTarget).children('ul')
+    $('h2').on('click', function (e) {
+      var elem2 = $(e.currentTarget).children('i')
+      $(elem).toggleClass('hidden')
+      $(elem2).toggleClass('glyphicon-chevron-down').toggleClass('glyphicon-chevron-up')
+    })
   })
 }
 
