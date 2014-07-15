32fad2b1645e9a113c33b984bc97add5d5baac90
Tomas Duringer Jacques de Lima <tomas.lima@dtmtec.com.br>
2014-07-15 14:43:04 -0300

    Corrige exibição das caixas de texto

--- app/models/accompaniment.rb
+++ app/models/accompaniment.rb
@@ -3,4 +3,11 @@ class Accompaniment < ActiveRecord::Base
   belongs_to :friend
 
   validates :subject, presence: true
+
+  def self.parse_html(field)
+    field = field.gsub("\n", "<br>")
+    field = field.gsub("\t", "&nbsp&nbsp")
+    field = field.gsub(" ", "&nbsp")
+    field.html_safe
+  end
 end
