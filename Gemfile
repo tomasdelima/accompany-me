9880b5f87862e39215552f5df48533eff7e31631
Tomas Duringer Jacques de Lima <tomas.lima@dtmtec.com.br>
2016-02-28 01:41:08 -0300

    Changes the login page

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rails'
 
-gem "therubyracer"
+gem 'therubyracer'
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+user = User.create(email: 'tomasdelima@gmail.com', password: 12345678)
+
+activitables = []
+accompaniables = []
+experienceables = []
+learnables = []
+
+(1..10).each do |n|
+  friend = User.create(email: "tomasdelima+friend_#{n}@gmail.com", password: 12345678)
+  Friendship.create(friend: friend, user: user)
+
+  activitables.push(friend)
+  accompaniables.push(friend)
+  experienceables.push(friend)
+  learnables.push(friend)
+end
+
+(1..20).each do |m|
+  activity = Activity.create(name: "Activity #{m}", activitable: activitables.sample)
+
+  accompaniables.push(activity)
+  experienceables.push(activity)
+  learnables.push(activity)
+end
+
+(1..40).each do |m|
+  accompaniment = Accompaniment.create(subject: "Accompaniment #{m}", accompaniable: activitables.sample)
+
+  experienceables.push(accompaniment)
+  learnables.push(accompaniment)
+end
+
+(1..80).each do |m|
+  experience = Experience.create(description: "Experience #{m}", experienceable: experienceables.sample)
+
+  learnables.push(experience)
+end
+
+(1..160).each do |m|
+  learning = Learning.create(summary: "Learning #{m}", learnable: learnables.sample)
+end
